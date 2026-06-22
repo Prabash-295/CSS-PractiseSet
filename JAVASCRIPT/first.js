41654a1bef2ea00120bdf7333cd400d6c6ee1f2e
Prabash-295 <prabashvarmamandapati@gmail.com>
2026-06-22 12:06:58 +0530

    created JAVASCRIPT folder, stored two objects information

---
+++ JAVASCRIPT/first.js
@@ -0,0 +1,17 @@
+const product = {
+  name : "Parker Jotter Standard CT Ball Pen (Black)",
+  ratingInStars : 4,
+  ratings: 7002,
+  isDeal: true,
+  price: 270,
+  MRP : 285,
+  offInPercentage : 5,
+};
+
+const profile = {
+  userName : "shradhakhapra",
+  posts : 195,
+  followers : "569k",
+  following : 4,
+  isFollow : false,
+};
