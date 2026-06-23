4833d1404ad356a78d8202b6e74dbaf2bce38d88
Prabash-295 <prabashvarmamandapati@gmail.com>
2026-06-23 14:40:02 +0530

    get practise of conditionals and ternary operator

--- javascriptHandson/first.js
+++ javascriptHandson/first.js
@@ -7,7 +7,6 @@ const product = {
   MRP : 285,
   offInPercentage : 5,
 };
-
 const profile = {
   userName : "shradhakhapra",
   posts : 195,
@@ -15,12 +14,37 @@ const profile = {
   following : 4,
   isFollow : false,
 };
-let mode = "dark-mode";
-let colour;
-if(mode==="dark-mode"){
-  colour="black";
+let num = 10;
+if(num%2===0){
+  console.log(num, "is Even Number");
 }
 else{
-  colour="white";
+  console.log(num, "is odd Number");
 }
-console.log(colour);
+let age = 17;
+age >= 18 ? console.log("adult") : console.log("Not Adult");
+let fiveMult = prompt("Enter a Number");
+if(fiveMult%5===0){
+  console.log("Yes, Divisible");
+}
+else{
+    console.log("No, not Divisible");
+}
+let Marks = prompt("Enter Your Marks To Check Grade: ");
+if(Marks>=80){
+  console.log("A grade");
+}
+else if(Marks>=70&&Marks<=79){
+  console.log("B grade");
+}
+else if(Marks>=60&&Marks<=69){
+  console.log("C grade");
+}
+else if(Marks>=50&&Marks<=59){
+  console.log("D grade");
+}
+else{
+  console.log("F grade");
+}
+let greet = prompt("Enter a Greeting: ");
+console.log(greet);
